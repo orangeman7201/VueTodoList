cf9ee0a5e62918dfe9bd761d05ef64deff561b50
unknown <aki.jazz1027@gmail.com>
2021-07-23 13:17:10 +0900

    Vue課題③微修正

--- src/store.js
+++ src/store.js
@@ -5,11 +5,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    todos: [
-      {task: "選択", state: "working"},
-      {task: "掃除", state: "finish"},
-      {task: "洗い物", state: "working"}
-    ]
+    todos: []
   },
   getters: {
     inputTodos: state => {
